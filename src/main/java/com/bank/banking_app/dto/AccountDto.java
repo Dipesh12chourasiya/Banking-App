0631dcf2c876d59d2ac8af7ebac0701e459c9c4b
Dipesh <dipeshchourasiya12@gmail.com>
2026-02-03 00:00:49 +0530

    basic api setup is completed

--- src/main/java/com/bank/banking_app/dto/AccountDto.java
+++ src/main/java/com/bank/banking_app/dto/AccountDto.java
@@ -1,26 +1,17 @@
-package com.bank.banking_app.entity;
-
-import jakarta.persistence.*;
+package com.bank.banking_app.dto;
 
 
-@Table(name="accounts")
-@Entity
-public class Account {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+public class AccountDto {
     private Long id;
-
-    @Column(name = "account_holder_name")
     private String accountHolderName;
-
     private double balance;
 
-    public Account(Long id, String accountHolderName, double balance) {
+    public AccountDto(Long id, String accountHolderName, double balance) {
         this.id = id;
         this.accountHolderName = accountHolderName;
         this.balance = balance;
     }
+
 
     public Long getId() {
         return id;
---
+++ src/main/java/com/bank/banking_app/service/AccountService.java
@@ -0,0 +1,9 @@
+package com.bank.banking_app.service;
+
+import com.bank.banking_app.dto.AccountDto;
+
+public interface AccountService {
+
+    AccountDto createAccount(AccountDto accountDto);
+
+}
